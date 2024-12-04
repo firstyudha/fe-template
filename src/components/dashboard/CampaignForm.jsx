09923c1583c376b882fe2011a901caa1fc8f9b3d
Intan.Safitri [ITB-SOE] <0467440422@bussan.co.id>
2024-12-04 09:08:13 +0700

    Delete controlid and import

--- src/components/dashboard/CampaignForm.jsx
+++ src/components/dashboard/CampaignForm.jsx
@@ -1,4 +1,4 @@
-import { CButton, CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle, CForm, CFormInput } from '@coreui/react';
+import { CButton, CFormInput } from '@coreui/react';
 import { Button } from '@mui/material';
 import React, { useState } from 'react';
 import Editor from 'react-simple-wysiwyg';
@@ -27,38 +27,41 @@ export default function CampaignForm({ addCampaign }) {
   return (
       <form className='p-0' onSubmit={handleInputChange}>
 
-        <CFormInput className="mb-3" controlId="exampleForm.ControlInput1"
+        <CFormInput className="mb-3"
           type="text"
           placeholder="Category"
           name="category"
           value={newProduct.category}
           onChange={(e) => setNewProduct({ ...newProduct, [e.target.name]: e.target.value })}></CFormInput>
     
-        <CFormInput className="mb-3" controlId="exampleForm.ControlInput1"
+        <CFormInput className="mb-3"
           type="text"
           placeholder="Product Desc"
           name="product_desc"
           value={newProduct.product_desc}
           onChange={(e) => setNewProduct({ ...newProduct, [e.target.name]: e.target.value })}></CFormInput>
-        <CFormInput className="mb-3" controlId="exampleForm.ControlInput1"
+
+        <CFormInput className="mb-3"
           type="number"
           placeholder="Product Price"
           name="product_price"
           value={newProduct.product_price}
           onChange={(e) => setNewProduct({ ...newProduct, [e.target.name]: e.target.value })}></CFormInput>
-        <CFormInput className="mb-3" controlId="exampleForm.ControlInput1"
+
+        <CFormInput className="mb-3"
            type="text"
            placeholder="Campaign Name"
            name="campaign_name"
            value={newProduct.campaign_name}
            onChange={(e) => setNewProduct({ ...newProduct, [e.target.name]: e.target.value })}></CFormInput>
+
         <Editor
             placeholder="Campaign Desc"
             name="campaign_desc"
             value={newProduct.campaign_desc}
             onChange={(e) => setNewProduct({ ...newProduct, [e.target.name]: e.target.value })}/>
         
-        <CFormInput className="mb-3" controlId="exampleForm.ControlInput1"
+        <CFormInput className="mb-3"
             type="file"
             accept="image/*"
             style={{ display: "none" }}
